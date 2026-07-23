26f2378c050cbfceb05f4d27a03df10d6dd39a3d
arfongce-ai <arfongce@gmail.com>
2026-07-23 16:39:27 +0900

    품새 전부 적용

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "poomsae-training-v60-multi-rule-reference";
+const CACHE_NAME = "poomsae-training-v64-textbook-bibliography";
 const APP_URL = "./index.html";
 const CORE_ASSETS = [
   "./",
