1b81b5bae11e1b70634de47e4d9ebb4626458288
arfongce-ai <arfongce@gmail.com>
2026-07-24 00:26:27 +0900

    광고별 링크와 노출 기간을 개별 설정

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "poomsae-training-v73-multi-banner";
+const CACHE_NAME = "poomsae-training-v75-independent-campaigns";
 const APP_URL = "./index.html";
 const CORE_ASSETS = [
   "./",
